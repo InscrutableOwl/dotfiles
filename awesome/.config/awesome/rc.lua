265531a58a7f0f40fd18845eccd449b58c17b1d9
InscrutableOwl <mttym42@icloud.com>
2025-05-01 11:39:29 -0400

    fix to rc.lua

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -583,4 +583,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Added startup programs
 
-awful.spawn.with_shell("picom")
+-- awful.spawn.with_shell("picom")
