81f9eee97b629a33c7d21b7239a57fb4c3df420a
Matt Marino <mttym42@icloud.com>
2025-05-03 15:23:11 -0400

    adding fedora setup

---
+++ fed_scripts/move.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+mkdir -p /home/mattm/.themes
+
+sudo cp -r /home/mattm/dotfiles/fonts/* /usr/share/fonts
+
+sudo cp -r /home/mattm/dotfiles/icons/* /usr/share/icons
+
+sudo cp -r /home/mattm/dotfiles/themes/* /usr/share/themes
+
+cp -r /home/mattm/dotfiles/themes/* /home/mattm/.themes
+
+
