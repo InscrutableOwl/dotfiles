2b41b82e4e56f0d03af0f13ab4b104445d0a1760
Matt Marino <mttym42@icloud.com>
2025-05-04 15:19:19 -0400

    removing extraneous files

--- fed_scripts/dnfp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## install dnf packages
-
-sudo dnf install -y lolcat cowsay htop fortune-mod innoextract curl sl vlc fastfetch stow btop dejavu-fonts-all sox audacious timeshift
--- fed_scripts/ghcli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Github CLI tools
-
-sudo dnf install dnf5-plugins
-sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install gh --repo gh-cli
