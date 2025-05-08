8a03e9d232e6c5188471d2271c5e5747df32a9b9
Matt Marino <mttym42@icloud.com>
2025-05-08 08:29:55 -0400

    configuring awesome and picom

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -583,4 +583,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- additions to config
 
-awful.spawn.with_shell("picom --animations -b")
+awful.spawn.with_shell("picom --experimental-backend -b")
