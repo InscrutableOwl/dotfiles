23ffda0a8701a910b3e2a499455dca2720951d77
InscrutableOwl <mttym42@icloud.com>
2026-01-22 15:26:39 -0500

    adding qtile and picom configs

---
+++ qtile/.config/qtile/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# polkit
+#  lxpolkit &
+
+# background
+nitrogen --restore &
+
+# compositor
+picom --animations -b &
+
+# Notifications
+# dunst -config ~/.config/qtile/dunst/dunstrc &
